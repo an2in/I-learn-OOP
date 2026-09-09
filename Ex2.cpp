736a061033c7c8eed27dfea3023669d169df0356
an2in <25520270@gm.uit.edu.vn>
2026-09-09 15:08:29 +0700

    feat: add Ex2

--- Ex2.cpp
+++ Ex2.cpp
@@ -12,18 +12,31 @@ protected:
 private:
   double storedCash;
 
+public:
   // Constructor
   Safe(string Owner, int SafeCode, double StoredCash)
       : owner(Owner), safeCode(SafeCode), storedCash(StoredCash) {}
 
   // Methods
   void deposit(double amount) {
-    if (amount > 0)
+    if (amount > 0) {
       storedCash += amount;
-    else
+      cout << "Okay, done bro!" << storedCash;
+    } else
       cout << "Actually, I can (or not) deduct money from your account but the "
               "problem statement dont allow me to do that:D";
   }
   double GetCash() const { return storedCash; }
 };
-int main() {}
+class MasterSafe : public Safe {
+public:
+  // Constructor
+  MasterSafe(string o, int s, double sC) : Safe(o, s, sC) {}
+  void changeCode(int newCode) { safeCode = newCode; }
+};
+int main() {
+  MasterSafe Test("Alice", 0, 1000);
+  Test.deposit(1000);
+  Test.changeCode(12);
+  cout << Test.owner << "\n";
+}
